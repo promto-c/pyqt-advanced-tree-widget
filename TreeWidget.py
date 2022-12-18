c210d527867cdd275ac38adf2bb1966ad0a9139f
promto-c <prommm01@gmail.com>
2022-12-18 15:00:47 +0700

    Use "from PyQt5 import QtWidgets” instead

--- TreeWidget.py
+++ TreeWidget.py
@@ -1,14 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem
+from PyQt5 import QtWidgets
 
-class TreeWidget(QTreeWidget):
+class TreeWidget(QtWidgets.QTreeWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         # Customize the widget as desired
         self.setHeaderLabel("Tree Widget")
 
 def main():
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     tree_widget = TreeWidget()
     tree_widget.show()
     sys.exit(app.exec_())
