f54a729bbecd4c8509a4caf5067ee220a70450e1
promto-c <prommm01@gmail.com>
2023-01-20 03:38:07 +0700

    simplify code

--- TablerQIcon.py
+++ TablerQIcon.py
@@ -15,19 +15,20 @@ class TablerQIcon:
     def __init__(self):
         ''' Initialize the widget and load the icons from the tabler-icons/icons directory.
         '''
-        for file in os.listdir(TABLER_ICONS_SVG_DIRECTORY):
-            
-            if file.endswith('.svg'):
-                # Use regex to replace invalid characters with an underscore
-                icon_name = re.sub(r'[^a-zA-Z0-9_]', '_', file.split('.')[0])
-                # prepend an underscore to the icon name if it starts with a number
-                if icon_name[0].isdigit():
-                    icon_name = "_" + icon_name
-                    
-                # join the directory path and the file name to get the full path of the icon file
-                icon_path = os.path.join(TABLER_ICONS_SVG_DIRECTORY, file)
-                # add the icon name and path to the icon_dict
-                self.icon_dict[icon_name] = icon_path
+        svg_file = [ file for file in os.listdir(TABLER_ICONS_SVG_DIRECTORY) if file.endswith('.svg') ]
+        
+        for file in svg_file:
+            # Use regex to replace invalid characters with an underscore
+            icon_name = re.sub(r'[^a-zA-Z0-9_]', '_', file.split('.')[0])
+
+            # prepend an underscore to the icon name if it starts with a number
+            if icon_name[0].isdigit():
+                icon_name = "_" + icon_name
+                
+            # join the directory path and the file name to get the full path of the icon file
+            icon_path = os.path.join(TABLER_ICONS_SVG_DIRECTORY, file)
+            # add the icon name and path to the icon_dict
+            self.icon_dict[icon_name] = icon_path
  
     def __getattr__(self, name: str) -> QtGui.QIcon:
         ''' Allows access to the icons as attributes by returning a QIcon object for a given icon name.
@@ -36,9 +37,7 @@ class TablerQIcon:
             Returns:
                 QIcon : QIcon object for the given icon name
         '''
-        if name in self.icon_dict.keys():
-            icon_path = self.icon_dict[name]
-            return QtGui.QIcon(icon_path)
+        return QtGui.QIcon(self.icon_dict.get(name))
 
 if __name__ == '__main__':
     # create instance
